e0cbba5863b49ed697576c577107fdd4c47bd5d7
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-11-19 12:53:43 -0800

    validations complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ActiveRecord::Base
+	validates :email,
+	presence: true,
+	email: true,
+	uniqueness: {case_sensitive: false}
+
+	validates :password
+	presence: true,
+	confirmation: true,
+	length: {minimum: 8}
+
+	validates :name,
+	presence: true
+
+	validates :phone,
+	presence: true,
+	length: {is: 10}
+
+	has_secure_password
+
+	def self.authenticate email, password
+		User.find_by_email(email).try(:authenticate, password)
+	end
+
 end

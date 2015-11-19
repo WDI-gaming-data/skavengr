c6c2ab04e1a330e00023c9ce7e205bfb451a5000
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-11-19 14:41:23 -0800

    validations

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,3 +1,17 @@
 class Quest < ActiveRecord::Base
+	validates :name,
+	presence: true
+
+	validates :owner_id,
+	presence: true
+
+	validates :start_date,
+	presence: true
+
+	validates :end_date, 
+	presence: true
+
+
+
 	has_and_belongs_to_many :users
 end

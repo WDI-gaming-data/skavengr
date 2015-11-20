3c806aee946e83768dab356bbd671b1b7a307fbf
Carl Reiner <creiner1@gmail.com>
2015-11-20 13:11:10 -0800

    modals for quest creation

--- app/assets/javascripts/quest.js
+++ app/assets/javascripts/quest.js
@@ -5,6 +5,7 @@ if(typeof gon === "undefined") {
 }
 
 var markers = [];
+var clues = [];
 var infoWindows = [];
 
 function initNewQuestMap() {
@@ -40,11 +41,13 @@ function createMarker(name, map) {
     infoWindow.open(map, this);
     // infoWindows.push(infoWindow);
   });
+  clues.push('');
   return marker;
 }
 
 function addFormRow() {
   var row = $('<li></li>').append($('<input>').attr('type', 'text').attr('idx', markers.length).addClass('loc'));
+  row.attr('name', 'user[name]');
   $('#loc-list').append(row);
   $('.loc').keyup(function(e) {
     var idx = parseInt(this.getAttribute('idx'), 10);
@@ -54,19 +57,16 @@ function addFormRow() {
 }
 
 //Function to package the locations into an array without the extra Google Maps marker data
-//Should spit out an object that can be stringified and passed to the server
+//Should be stringified and passed to the server
 function packageMarkers(arr) {
-  //var questName = value of some input, or param passed to this page
-  var locations = arr.map(function(marker) {
+  var locations = arr.map(function(marker, idx) {
     var rObj = {};
     rObj.lat = marker.getPosition.lat();
     rObj.lng = marker.getPosition.lng();
     rObj.name = marker.label;
+    rObj.clue = clues[idx];
   });
-  return {
-    //name: questName,
-    locations: locations
-  };
+  return locations;
 }
 
 $(function() {
@@ -75,10 +75,10 @@ $(function() {
     addFormRow();
     markers.push(createMarker('test', map));
   });
-  $('.loc').keyup(function(e) {
-    console.log('in keyup');
-    console.log(this);
-    markers[parseInt(this.attr('idx'))].setLabel(this.val());
-  });
+  // $('.loc').keyup(function(e) {
+  //   console.log('in keyup');
+  //   console.log(this);
+  //   markers[parseInt(this.attr('idx'))].setLabel(this.val());
+  // });
 });
 

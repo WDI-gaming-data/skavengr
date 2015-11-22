bdc9ceadfdf0be8412cabfefebf42eb0f487f006
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-11-21 21:28:45 -0800

    quest creation almost complete

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -12,8 +12,19 @@ class QuestsController < ApplicationController
   end
 
   def create
-  	Quest.create(:name => "testaquest", :owner_id=>1)
-  	redirect_to new_quest_path
+    # render :json => params
+    quest_parameters = params[:quest]
+    locations = JSON.parse quest_parameters[:locations]
+    heroes = JSON.parse quest_parameters[:heroes]
+  	quest = Quest.create(:name => "test", :owner_id=>1, :start_date=>quest_parameters['start'], :end_date=>quest_params['end'])
+  	locations.each do |location|
+      quest.locations.create name: location['name'], clue: location['clue'], lat: location['lat'], lng: location['lng'];
+    end
+    heroes.each do |hero|
+      hero = User.find_by email: hero
+      puts hero
+    end
+    redirect_to root_path
   end
 
   def new
@@ -22,6 +33,6 @@ class QuestsController < ApplicationController
   private
 
   def quest_params
-  	params.require(:quests).permit(:name, :owner_id, :start_date, :end_date)
+  	params.require(:quest).permit(:name, :owner_id, :start, :end)
   end
 end

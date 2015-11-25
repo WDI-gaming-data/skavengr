b8c140e14a679afd8320da9721f0322d42c993b6
Alec M <alecmcgovern444@gmail.com>
2015-11-24 17:22:36 -0800

    mobile

--- app/assets/javascripts/quest.js
+++ app/assets/javascripts/quest.js
@@ -226,23 +226,23 @@ $(function() {
     setTimeout(google.maps.event.trigger, 500, map, "resize");
   });
 
-   $window = $(window);
+   // $window = $(window);
  
-   $('section[data-type="background"]').each(function(){
-     // declare the variable to affect the defined data-type
-     var $scroll = $(this);
+   // $('section[data-type="background"]').each(function(){
+   //   // declare the variable to affect the defined data-type
+   //   var $scroll = $(this);
                      
-      $(window).scroll(function() {
-        // HTML5 proves useful for helping with creating JS functions!
-        // also, negative value because we're scrolling upwards                             
-        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+   //    $(window).scroll(function() {
+   //      // HTML5 proves useful for helping with creating JS functions!
+   //      // also, negative value because we're scrolling upwards                             
+   //      var yPos = -($window.scrollTop() / $scroll.data('speed')); 
          
-        // background position
-        var coords = '50% '+ yPos + 'px';
+   //      // background position
+   //      var coords = '50% '+ yPos + 'px';
  
-        // move the background
-        $scroll.css({ backgroundPosition: coords });    
-      }); // end window scroll
-   });
+   //      // move the background
+   //      $scroll.css({ backgroundPosition: coords });    
+   //    }); // end window scroll
+   // });
 });
 
